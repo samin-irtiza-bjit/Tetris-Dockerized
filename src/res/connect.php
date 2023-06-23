2f3d98e519514e0fb19cec671b5490fe05eaa318
Md. Samin Irtiza <131145579+samin-irtiza-bjit@users.noreply.github.com>
2023-06-23 09:54:56 +0600

    Update connect.php

--- src/res/connect.php
+++ src/res/connect.php
@@ -3,7 +3,7 @@ $username = "tetris";
 $password = "tetrispass";
 $dbname = "tetris";
 // Create connection
-$conn = new mysqli("db-server", $username, $password, $dbname);
+$conn = new mysqli("mysql-service", $username, $password, $dbname);
 // Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
